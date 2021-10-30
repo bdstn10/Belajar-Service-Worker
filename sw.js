50eca98ee031647e6df3c92fae8e6caacd4749bc
budisetiawan-web <budi958585@gmail.com>
2021-10-30 22:35:17 +0700

    Merubah teknik cache dari cache first menjadi network first

--- sw.js
+++ sw.js
@@ -22,37 +22,21 @@ self.addEventListener("install", e => {
 
 let respond = new Response("Hallo Hengkermannn")
 self.addEventListener("fetch", e => {
+    let cloneRequest = e.request.clone();
+
     e.respondWith(
-        caches.open(cacheName)
-        .then(cache => {
-            return cache.match(e.request).then(response => {
-                if (response) {
-                    return response
-                }
+        fetch(e.request.clone()).then(r => {
+            cloneResponse = r.clone();
 
-                let cloneRequest = e.request.clone();
-                return fetch(e.request)
-                    .then(
-                        r => {
-                            let cloneResponse = r.clone()
-                            console.log(r.status);
-                            if (r.status != 404) {
-                                // console.log("Request berhasil");
-                                caches.open(cacheName)
-                                    .then(cache => {
-                                        cache.put(cloneRequest, cloneResponse);
-                                    })
-
-                                return r;
-
-                            } else {
-                                // console.log('Request e gagal mas');
-                                return fetch("http://localhost:5500/fallback.html");
-                            }
-                        },
-                        e => console.log(e)
-                    );
+            caches.open(cacheName).then(cache => {
+                cache.put(cloneRequest, cloneResponse)
             })
+
+            return r
+        })
+        .catch(async function() {
+            let cache = await caches.open(cacheName)
+            return cache.match(e.request)
         })
     )
 })
